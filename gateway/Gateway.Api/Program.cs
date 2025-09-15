8dd49cbd062c3439d42496ef80542a6e8a8d71af
Tetri Mesquita Neto <tetri.neto@aliare.co>
2025-09-15 18:51:16 -0300

    feat(gateway): implement YARP reverse proxy gateway

--- gateway/Gateway.Api/Program.cs
+++ gateway/Gateway.Api/Program.cs
@@ -1,12 +1,10 @@
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddReverseProxy()
+    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    //app.MapOpenApi();
-}
-
-app.UseHttpsRedirection();
+app.MapReverseProxy();
 
 app.Run();
